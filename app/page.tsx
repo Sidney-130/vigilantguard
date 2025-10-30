83d095f31844a64869db44c719b2e9323a234882
Sidney-130 <akabuoguoluebube8@gmail.com>
2025-10-30 04:05:06 +0100

    add  use client to page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+"use client";
+
 import { useRouter } from "next/navigation";
 
 export default function Home() {
   const router = useRouter();
   return (
-    <div className="flex flex-col justify-center items-center">
+    <div className="flex flex-col justify-center items-center bg-white">
       <button className="bg-blue-600 p-4" onClick={() => router.push("/login")}>
         Get Started
       </button>
