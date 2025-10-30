bb61a3eddf70c9199491e4fd9b16901fe1dc0b8f
Sidney-130 <akabuoguoluebube8@gmail.com>
2025-10-30 04:06:10 +0100

    add bg-white

--- app/page.tsx
+++ app/page.tsx
@@ -5,8 +5,11 @@ import { useRouter } from "next/navigation";
 export default function Home() {
   const router = useRouter();
   return (
-    <div className="flex flex-col justify-center items-center bg-white">
-      <button className="bg-blue-600 p-4" onClick={() => router.push("/login")}>
+    <div className="flex flex-col justify-center items-center bg-white h-screen">
+      <button
+        className="bg-blue-600 p-4 rounded-md text-white"
+        onClick={() => router.push("/login")}
+      >
         Get Started
       </button>
     </div>
